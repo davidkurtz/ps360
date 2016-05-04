45132887fb6ee4c1fab4f6669a6ea3d9205fc026
davidkurtz <info@go-faster.co.uk>
2016-05-04 22:17:40 +0100

    minor fixes
    use listagg to simplify gtt queries.  Omit recyclebin from descending
    index queries

--- psaetempgttnotcand.sql
+++ psaetempgttnotcand.sql
@@ -8,15 +8,7 @@ DEF descrlong = 'Temporary Records that are marked as GTTs from PeopleTools 8.54
 set lines 200 pages 99
 BEGIN
   :sql_text := '
-WITH a AS (
-SELECT /*+MATERIALIZE*/ t.recname, t.ae_applid, d.temptblinstances
-,      ROW_NUMBER() OVER (PARTITION BY t.recname ORDER BY t.ae_applid) AS curr
-,      ROW_NUMBER() OVER (PARTITION BY t.recname ORDER BY t.ae_applid)-1 AS prev
-FROM   psaeappltemptbl t
-,      psaeappldefn d        
-WHERE  t.ae_applid = d.ae_applid
-AND    d.ae_disable_restart = ''N''
-), x as (
+WITH x as (
 select r.recname
 ,      (SELECT COUNT(DISTINCT d.ae_applid)
        FROM    psaeappltemptbl t
@@ -26,10 +18,12 @@ select r.recname
        AND     t.recname = r.recname
        ) numrestartae
 ,      (
-       SELECT SUBSTR(LTRIM(MAX(SYS_CONNECT_BY_PATH(a.ae_applid||'' (''||a.temptblinstances||'')'','', '')) KEEP (DENSE_RANK LAST ORDER BY a.curr),'',''),2)
-       FROM   a
-       CONNECT BY a.prev = PRIOR a.curr AND a.recname = PRIOR a.recname
-       START WITH a.curr = 1 AND a.recname = r.recname
+       SELECT LISTAGG(t.ae_applid||'' (''||d.temptblinstances||'')'','', '') WITHIN GROUP (ORDER BY t.ae_applid)
+       FROM   psaeappltemptbl t
+       ,      psaeappldefn d
+       WHERE  t.ae_applid = d.ae_applid
+       AND    d.ae_disable_restart = ''N''
+       AND    t.recname = r.recname
        ) ae_applids
 from   psrecdefn r
 where  r.rectype = 7
