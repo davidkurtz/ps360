18c47e5eb649f4b3339e340a379a077938b2f105
David Kurtz <info@go-faster.co.uk>
2022-01-18 15:54:25 +0000

    Update psobjdbnotps.sql
    Add index handling.
    Improved handling of PeopleSoft tables and indexes associated temporary records

--- psobjdbnotps.sql
+++ psobjdbnotps.sql
@@ -16,30 +16,59 @@ FROM   all_objects o
 WHERE  o.object_type IN(''TABLE'',''VIEW'',''MATERIALIZED VIEW'')
 AND    o.owner = ''&&ownerid''
 AND    NOT object_name IN(''MV_CAPABILITIES_TABLE'')
-minus
-SELECT CASE WHEN r.rectype IN(0,7) THEN ''TABLE''
-            WHEN BITAND(r.auxflagmask,16777216) = 16777216 THEN ''TABLE''
-            WHEN r.rectype IN(1,6) THEN ''VIEW''
-       END as object_type
-,      DECODE(r.sqltablename,'' '',''PS_''||r.recname,r.sqltablename)
-FROM   psrecdefn r
-WHERE  r.rectype IN(0,1,6,7)
+MINUS
+SELECT ''TABLE'' object_type
+,      DECODE(r.sqltablename, '' '', ''PS_''||r.recname,r.sqltablename)||DECODE(n.n,0,'''',n.n) table_name
+FROM   n
+,      psrecdefn r
+       LEFT OUTER JOIN pstemptblcntvw c ON r.recname = c.recname
+,      psoptions o
+WHERE  (n.n = 0 OR (r.rectype = 7 /*TEMPORARY TABLE*/ AND n.n <= NVL(c.temptblinstances,0)+o.temptblinstances))
+AND    r.rectype IN(0,1,6,7)
+MINUS
+SELECT ''INDEX'' object_type
+,      ''PS_''||DECODE(r.sqltablename, '' '', r.recname,r.sqltablename)||DECODE(n.n,0,'''',n.n) table_name
+FROM   n
+,      psrecdefn r
+       LEFT OUTER JOIN pstemptblcntvw c ON r.recname = c.recname
+,      psoptions o
+,      psrecfielddb f
+WHERE  (n.n = 0 OR (r.rectype = 7 /*TEMPORARY TABLE*/ AND n.n <= NVL(c.temptblinstances,0)+o.temptblinstances))
+AND    r.rectype IN(0,7)
+AND    r.recname = f.recname
+and    bitand(f.useedit,3)>0
+MINUS 
+SELECT ''INDEX'' object_type
+,      ''PS''||CHR(47+row_number() over (partition by r.recname, n.n ORDER BY f.fieldnum))
+           ||DECODE(r.sqltablename, '' '', r.recname,r.sqltablename)||DECODE(n.n,0,'''',n.n) table_name
+FROM   n
+,      psrecdefn r
+       LEFT OUTER JOIN pstemptblcntvw c ON r.recname = c.recname
+,      psoptions o
+,      psrecfielddb f
+WHERE  (n.n = 0 OR (r.rectype = 7 /*TEMPORARY TABLE*/ AND n.n <= NVL(c.temptblinstances,0)+o.temptblinstances))
+AND    r.rectype IN(0,7)
+AND    r.recname = f.recname
+and    bitand(f.useedit,16)>0
+MINUS
+SELECT ''INDEX'' object_type
+,      ''PS''||i.indexid||DECODE(r.sqltablename, '' '', r.recname,r.sqltablename)||DECODE(n.n,0,'''',n.n) table_name
+FROM   n
+,      psrecdefn r
+       LEFT OUTER JOIN pstemptblcntvw c ON r.recname = c.recname
+,      psoptions o
+,      psindexdefn i
+WHERE  (n.n = 0 OR (r.rectype = 7 /*TEMPORARY TABLE*/ AND n.n <= NVL(c.temptblinstances,0)+o.temptblinstances))
+AND    r.rectype IN(0,7)
+AND    i.recname = r.recname
+AND    i.activeflag = 1
+AND    i.platform_ora = 1
 MINUS
 SELECT ''MATERIALIZED VIEW'' object_type
 ,      DECODE(r.sqltablename,'' '',''PS_''||r.recname,r.sqltablename)
 FROM   psrecdefn r
 WHERE  r.rectype = 1 /*views*/
 AND    BITAND(r.auxflagmask,16777216) = 16777216 /*MV*/
-MINUS
-SELECT ''TABLE'' object_type
-,      DECODE(r.sqltablename, '' '', ''PS_''||r.recname,r.sqltablename)||DECODE(n.n,0,'''',n.n) table_name
-FROM   n
-,      pstemptblcntvw c
-,      psrecdefn r
-,      psoptions o
-WHERE  r.recname = c.recname
-AND    r.rectype = 7 /*TEMPORARY TABLE*/
-AND    n.n <= c.temptblinstances+o.temptblinstances
 )
 SELECT row_number() OVER (ORDER BY object_name, object_type) row_num
 ,      x.*
